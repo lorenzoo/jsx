3ef0a1f799888264cb976f92f23c25fe43dd3ff6
enrique lorenzo <enriquelorenzoo@gmail.com>
2023-03-23 20:20:51 +0100

    trabajando con query y where en las categorias

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { db } from "../../FirebaseConfig";
 //import { products } from "../../ProductsMock";
 import ItemList from "../ItemList/ItemList";
-import {collection, getDocs} from "firebase/firestore"
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const ItemListContainer = ({ title }) => {
   const { categoryId } = useParams();
@@ -11,36 +11,32 @@ const ItemListContainer = ({ title }) => {
 
   const [items, setItems] = useState([]);
 
- // const productosFiltrados = products.filter(
- //   (elemento) => elemento.category === categoryId
- // );
-
+ 
   useEffect(() => {
-  //  const productList = new Promise((resolve, reject) => {
-  //   resolve(categoryId ? productosFiltrados : products);
-  // });
-  //  productList
-  //    .then((res) => {
-  //      setItems(res);
-  //    })
-  //    .catch((error) => {
-  //     console.log(error);
-  //    });
-// ARREGLAR ESTA PARTE QUE NO FUNCIONA 
- const itemsCollection = collection(db, "products");
- getDocs(itemsCollection).then((res) => {
-    let products = res.docs.map((product) => {
-      return {
-       ...product.data(),
-       id: product.id
-     };
-    });
-   setItems(products);
+    const itemsCollection = collection(db, "products");
+    // ARREGLAR ESTA PARTE QUE NO FUNCIONA
+    let consulta = undefined ;
+
+
+    if (categoryId) {
+      const q = query(itemsCollection, where("category", "==", categoryId));
+      consulta = getDocs(q)
+
+    } 
+    else {
+     consulta = getDocs(itemsCollection)
+
+    }
+consulta.then((res)=> {
+let products = res.docs.map((product)=> {
+  return {
+    ...product.data(),
+    id: product.id
+  }
 })
-   
-    },
-  [categoryId]);
-    
+setItems(products);
+})
+  }, [categoryId]);
 
   return (
     <div>
